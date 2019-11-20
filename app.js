b14d583da65cb9d6f34bbb0c2a9f4f7c1d128f7c
Nathanael Reges <nathanaelreges@gmail.com>
2019-11-20 08:24:21 -0300

    testing

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
+  console.log('sdasdasd');
   res.send('Hello World')
 })
+
+
+app.post('/*', function(req, res) {
+  console.log(req.body);
+  res.sendStatus(200);
+});
 
 app.listen(3210)
