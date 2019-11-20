dfdf4b5c3705319a8e6a56e48414fe93b9c14d7b
Nathanael Reges <nathanaelreges@gmail.com>
2019-11-20 15:53:51 -0300

    admin improvements

--- src/chatController.js
+++ src/chatController.js
@@ -7,31 +7,38 @@ const activeChannels = {
   get() {
     return this._channels;
   },
-  _channels: {},
-};
-
-const categoryHandlers = {
-  'group_channel:message_send'({ sender, channel, payload }) {
-    if (sender.user_id === 'student_guide') return;
-
-    const entry = {
-      userId: sender.user_id,
-      nickname: sender.nickname,
-      channelUrl: channel.channel_url,
-      lastMessage: payload.message
-    };
-
-    activeChannels.put(entry)
+  _channels: {
+    "sendbird_group_channel_181293707_881e086530540d77b713696a8512a355f359801c": {
+      "userId": "nathanael1234",
+      "nickname": "natha",
+      "channelUrl": "sendbird_group_channel_181293707_881e086530540d77b713696a8512a355f359801c",
+      "lastMessage": "kljasdlkjasd"
+    },
   }
 }
+
+  const categoryHandlers = {
+    'group_channel:message_send'({ sender, channel, payload }) {
+      if (sender.user_id === 'student_guide') return;
+
+      const entry = {
+        userId: sender.user_id,
+        nickname: sender.nickname,
+        channelUrl: channel.channel_url,
+        lastMessage: payload.message
+      };
+
+      activeChannels.put(entry)
+    }
+  }
 
 module.exports = {
-  handle(body) {
-    if (categoryHandlers[body.category]) {
-      categoryHandlers[body.category](body)
+    handle(body) {
+      if (categoryHandlers[body.category]) {
+        categoryHandlers[body.category](body)
+      }
+    },
+    getChannels() {
+      return activeChannels.get()
     }
-  },
-  getChannels() {
-    return activeChannels.get()
   }
-}
